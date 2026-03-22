d08964a1e1cdab156130aa51ecd1c23f74ed4009
Ericsson <ericsson_eduardo@hotmail.com>
2026-03-22 20:48:05 -0300

    identação

--- somaneg.py
+++ somaneg.py
@@ -9,7 +9,11 @@ os.environ["HADOOP_HOME"] = r"C:\hadoop\bin"
 
 RU=3602977
 
-spark = SparkSession.builder.appName("Trabalho de Big DATA").config("spark.driver.extraJavaOptions", "--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED").config("spark.executor.extraJavaOptions", "--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED").config("spark.memory.offHeap.enabled","true").config("spark.memory.offHeap.size","2g").getOrCreate()
+spark = SparkSession.builder.appName("Trabalho de Big DATA").config("spark.driver.extraJavaOptions", "--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED")\
+    .config("spark.executor.extraJavaOptions", "--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED")\
+    .config("spark.memory.offHeap.enabled","true")\
+    .config("spark.memory.offHeap.size","2g")\
+    .getOrCreate()
 
 df = spark.read.format("csv") \
     .option("header", "true") \
@@ -18,10 +22,12 @@ df = spark.read.format("csv") \
     .option("escape", "\"") \
     .option("quote", "\"") \
     .load("archive/imdb-reviews-pt-br.csv")
+
 df.show(5,0)
 
 dfneg = df.filter(df.sentiment == "neg")
-print(dfneg.count())
+print(f"numero de colunas com sentimento NEG:",dfneg.count())
 somaIDneg = dfneg.select(F.sum("id")).collect()[0][0]
 
-print(somaIDneg)
+print(f"Soma dos IDs de linhas dos com sentimento NEG :",somaIDneg)
+print(f"RU:",RU)
