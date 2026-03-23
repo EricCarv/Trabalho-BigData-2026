25b634e520d07645a50cdde8b5a0fe6462f4e40e
Ericsson <ericsson_eduardo@hotmail.com>
2026-03-22 21:37:44 -0300

    alterada logica e versão do python

--- somaneg.py
+++ somaneg.py
@@ -1,11 +1,13 @@
 #import setuptools
 import os
+import sys
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as F
 
 os.environ["JAVA_HOME"] = r"C:\Program Files\Java\jdk-17"
 os.environ["PATH"] += os.pathsep + os.path.join(os.environ["JAVA_HOME"], "bin")
 os.environ["HADOOP_HOME"] = r"C:\hadoop\bin"
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
 RU=3602977
 
@@ -23,11 +25,31 @@ df = spark.read.format("csv") \
     .option("quote", "\"") \
     .load("archive/imdb-reviews-pt-br.csv")
 
+'''
+print("passou")
 df.show(5,0)
+print("passou")
+'''
 
-dfneg = df.filter(df.sentiment == "neg")
-print(f"numero de colunas com sentimento NEG:",dfneg.count())
-somaIDneg = dfneg.select(F.sum("id")).collect()[0][0]
+def map1(x):
+    try:
+        return x['sentiment'], int(x['id'])
+    except:
+        return x['sentiment'], 0
 
-print(f"Soma dos IDs de linhas dos com sentimento NEG :",somaIDneg)
+def reduceByKey1(x,y):
+    return x+y
+
+resultadoreduce=df.rdd.map(map1).reduceByKey(reduceByKey1).collect()
+
+for sentimento, soma in resultadoreduce:
+    if sentimento == "neg":
+        print(f"\n Sentimento selecionado: {sentimento}")
+        print(f"Soma dos IDs de linhas com sentimento NEG: {soma}")
+
+print("resultadoreduce:",resultadoreduce)
+
+
+
 print(f"RU:",RU)
+print()
